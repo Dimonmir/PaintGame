59aaeec1abf755265a06f04df9ee03a1a6ccda85
Lavrentyev Dmitriy <dmitriy.lavrentyev@itecorp.ru>
2023-08-04 17:44:36 +0300

    fix react

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import svgrPlugin from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({
-    jsxRuntime: 'classic' // Add this line
+    jsxRuntime: 'automatic' // Add this line
   }), viteTsconfigPaths(), svgrPlugin()],
 })
