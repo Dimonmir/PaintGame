ea332744050a8db27ffdf7c97bb3f6adbb5bb2d9
Lavrentyev Dmitriy <dmitriy.lavrentyev@itecorp.ru>
2023-10-02 17:56:16 +0300

    feat: добавлен canvas

--- src/features/canvas/ui/Canvas.tsx
+++ src/features/canvas/ui/Canvas.tsx
@@ -1,5 +1,59 @@
+import { useEffect, useRef } from 'react';
 import { SCanvas } from './s-canvas';
 
 export const Canvas = () => {
-  return <SCanvas />;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext('2d');
+    if (!context) return;
+
+    context.scale(2, 2);
+    context.lineCap = 'round';
+    context.strokeStyle = 'black';
+    context.lineWidth = 5;
+    contextRef.current = context;
+  }, []);
+
+  const startDrawing = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+    const { offsetX, offsetY } = nativeEvent;
+
+    if (!contextRef.current) return;
+
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+  };
+
+  const draw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+    const { offsetX, offsetY } = nativeEvent;
+
+    if (!contextRef.current) return;
+
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
+  return (
+    <SCanvas
+      ref={canvasRef}
+      onMouseDown={startDrawing}
+      onMouseMove={draw}
+      onMouseUp={() => {
+        if (contextRef.current) {
+          contextRef.current.closePath();
+        }
+      }}
+    />
+  );
 };
+
+export default Canvas;
--- src/features/canvas/ui/s-canvas.ts
+++ src/features/canvas/ui/s-canvas.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-export const SCanvas = styled.div`
+export const SCanvas = styled.canvas`
     flex: 1 1 80%;
     border-right: 1px solid ${({theme}) => theme.colors.lightGray};
 `
